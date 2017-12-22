a7490fe5658458a50dca9618880668b613fd08a6
KelseyJ22 <kelsey2@stanford.edu>
2017-12-22 13:42:26 -0800

    initial commit

---
+++ analyze_weather.py
@@ -0,0 +1,7 @@
+import utils
+
+import numpy as np
+
+all_weather = read_data()
+report_basic_stats()
+rain_by_year()
---
+++ utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+# structure: location number, temperature, dewpoint/temperature differential, visibility, precipitation, wind speed
+# Indicators (1 = yes, 0 = no/not          
+#                         reported) for the occurrence during the 
+#                         day of:
+#                         Fog ('F' - 1st digit).
+#                         Rain or Drizzle ('R' - 2nd digit).
+#                         Snow or Ice Pellets ('S' - 3rd digit).
+#                         Hail ('H' - 4th digit).
+#                         Thunder ('T' - 5th digit).
+#                         Tornado or Funnel Cloud ('T' - 6th       
+#                         digit).
+
+def is_october():
+
+def parse_date():
+
+def read_data():
+	# for file in folder
+	#	for line in file:
+			# split into datapoints
+			# determine if relevant
+			# throw away not October
+			# otherwise store in structure
+	# return structure
