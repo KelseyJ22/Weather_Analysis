21c8a504e0e988003f0b08dee3d98b9d3cec3e2d
KelseyJ22 <kelsey2@stanford.edu>
2017-12-24 17:44:27 -0800

    very basic analysis stuff

--- analyze_weather.py
+++ analyze_weather.py
@@ -13,13 +13,18 @@ precip = 8
 codes = 9
 
 def report_basic_stats(all_weather):
-	pass
-# number of days it rains, on average
-# inches of rain in October, on average
-# rainiest year by days and by amount
-# number of days with low visibility
-# average visibility
-# rainiest weather station, on average
+	print all_weather.shape
+	# number of days it rains, on average
+	# inches of rain in October, on average
+	# rainiest year by days and by amount
+	# number of days with low visibility
+	# average visibility
+	print np.mean(all_weather, axis=visibility)
+	# average amount of rain
+	print np.mean(all_weather, axis=precip)
+	# average temperature
+	print np.mean(all_weather, axis=temp)
+	# rainiest weather station, on average
 
 
 all_weather = read_data()
